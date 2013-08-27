48dd3e80bf6c421469ad2d4aa5692af1f266248b
Mickey Ngo <nanyangbk@gmail.com>
2013-08-27 21:02:36 +0700

    add bew textarea ti display result

--- client/index.js
+++ client/index.js
@@ -1,14 +1,13 @@
+Template.text_result.result = function() {	
+	var txt = texts.findOne();
+	if (txt) return txt["text"];
+	return "";
+};
 Template.text_area.result = function() {	
 	var txt = texts.findOne();
 	if (txt) return txt["text"];
 	return "";
-	// if (text_collection) {
-	// 	var text = text_collection["text"];
-	// 	if (text) return text;
-	// }
-	// return "";
 };
-
 Template.text_area.events({
 	'keyup #main_text': function(e, templ) {		
 		if (e.which === 13) {
